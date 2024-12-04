05daa5e3a564a693d7a4ab81258921866ced5999
Shashank Aditya <msrsaditya@gmail.com>
2024-12-05 01:14:14 +0530

    Create Command.sh

---
+++ Installation/Command.sh
@@ -0,0 +1,5 @@
+# Full Links
+archinstall --config https://raw.githubusercontent.com/msrsaditya/Arch-Cinnamon/refs/heads/main/Installation/user_configuration.json --creds https://raw.githubusercontent.com/msrsaditya/Arch-Cinnamon/refs/heads/main/Installation/user_credentials.json
+
+# Bitly Shortened Links
+archinstall --config https://bit.ly/4f10PCf --creds https://bit.ly/3D9cxxb
