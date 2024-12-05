0e4c0deeea1cc0acc67c341d38fc3b68732f1aa9
Shashank Aditya <msrsaditya@gmail.com>
2024-12-05 21:28:36 +0530

    Update Command.sh

--- Installation/Command.sh
+++ Installation/Command.sh
@@ -1,5 +1,12 @@
+## Pre-Installation
+
 # Full Links
 archinstall --config https://raw.githubusercontent.com/msrsaditya/Arch-Cinnamon/refs/heads/main/Installation/user_configuration.json --creds https://raw.githubusercontent.com/msrsaditya/Arch-Cinnamon/refs/heads/main/Installation/user_credentials.json
 
 # Bitly Shortened Links
 archinstall --config https://bit.ly/4f10PCf --creds https://bit.ly/3D9cxxb
+
+## Post-Installation
+wget "https://raw.githubusercontent.com/msrsaditya/Arch-Cinnamon/refs/heads/main/Installation/PostInstall.sh"
+sudo chmod +x PostInstall.sh
+sudo bash PostInstall.sh
