c95b4071abc799c1dfd0dda185dfce16a32b80cf
Shashank Aditya <msrsaditya@gmail.com>
2024-12-07 13:49:59 +0530

    Update InstallCommand.sh

--- Installation/InstallCommand.sh
+++ Installation/InstallCommand.sh
@@ -7,7 +7,7 @@ archinstall --config https://bit.ly/4ii7E5n --creds https://bit.ly/4fUn4uO
 # Post-Installation
 
 wget "https://raw.githubusercontent.com/msrsaditya/Arch-Cinnamon/refs/heads/main/Installation/PostInstallation/PostInstall.sh"
-wget "https://bit.ly/3Vqxlqr"
+wget --content-disposition "https://bit.ly/3Vqxlqr"
 
 sudo chmod +x PostInstall.sh
 sudo bash PostInstall.sh
