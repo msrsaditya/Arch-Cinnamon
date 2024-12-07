56e3f9763a87f758e6192365f263b8753ed859ca
Shashank Aditya <msrsaditya@gmail.com>
2024-12-07 18:37:58 +0530

    Update PostInstall.sh

--- Installation/PostInstallation/PostInstall.sh
+++ Installation/PostInstallation/PostInstall.sh
@@ -3,19 +3,18 @@
 ## Package Management
 
 # Update Everything and Clean
-pacman -Syyu --noconfirm && pacman -Rns --noconfirm $(pacman -Qdtq)
+sudo pacman -Syyu --noconfirm && sudo pacman -Rns --noconfirm $(pacman -Qdtq)
 
 # Pacman Configuration Edit
-sed -i '/^#Color/s/^#//' /etc/pacman.conf
-sed -i 's/^#*ParallelDownloads.*/ParallelDownloads = 10/' /etc/pacman.conf
-sed -i '/ParallelDownloads = 10/a ILoveCandy' /etc/pacman.conf
-sed -i 's/^#MAKEFLAGS="-j2"/MAKEFLAGS="-j4"/' /etc/makepkg.conf
+sudo sed -i '/^#Color/s/^#//' /etc/pacman.conf
+sudo sed -i 's/^#*ParallelDownloads.*/ParallelDownloads = 10/' /etc/pacman.conf
+sudo sed -i '/ParallelDownloads = 10/a ILoveCandy' /etc/pacman.conf
+sudo sed -i 's/^#MAKEFLAGS="-j2"/MAKEFLAGS="-j4"/' /etc/makepkg.conf
 
 # Install Necessary Applications from Official Repos
-pacman -Syyu --noconfirm alacritty aria2 bleachbit cinnamon curl eog evince fastfetch ffmpeg git gvfs-mtp htop lf libreoffice-fresh lightdm man-db mpv mtpfs networkmanager nemo neovim noto-fonts-emoji openssh otf-font-awesome pandoc-cli p7zip reflector tar touchegg ttf-jetbrains-mono-nerd unrar unzip upower vbetool wget wireless_tools xclip xed xdg-utils xdg-user-dirs yt-dlp zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -Syyu --noconfirm alacritty aria2 bleachbit cinnamon curl eog evince fastfetch ffmpeg fzf git gvfs-mtp htop lf libreoffice-fresh lightdm man-db mpv mtpfs networkmanager nemo neovim noto-fonts-emoji openssh otf-font-awesome pandoc-cli p7zip reflector tar touchegg ttf-jetbrains-mono-nerd unrar unzip upower vbetool wget wireless_tools xclip xed xdg-utils xdg-user-dirs yt-dlp zsh zsh-autosuggestions zsh-syntax-highlighting
 
 # Install Yay AUR Helper
-sudo -u user bash <<EOF
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
@@ -24,24 +23,38 @@ rm -rf yay
 
 # Install Necessary Applications from AUR
 yay -S --noconfirm brave-bin file-roller-linuxmint gnome-calculator-gtk3 jmtpfs mint-themes mint-y-icons
-EOF
 
-## Login Behavior
+### Login Behavior
+
+## Bootloader
 
 # Turn Off Mitigations and Skip Grub on Login
-sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/"$/ mitigations=off"/' /etc/default/grub
-sed -i '/^GRUB_TIMEOUT=/d' /etc/default/grub
-echo "GRUB_TIMEOUT=0" >> /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/"$/ mitigations=off"/' /etc/default/grub
+sudo sed -i '/^GRUB_TIMEOUT=/d' /etc/default/grub
+echo "GRUB_TIMEOUT=0" | sudo tee -a /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+## TTY
 
 # Login to TTY, not GUI by Default
-systemctl set-default multi-user.target
+sudo systemctl set-default multi-user.target
 
-# Setup Auto Login in LightDM (if using GUI as default)
-sed -i '/^autologin-user=/d; /^autologin-user-timeout=/d; /^autologin-session=/d' /etc/lightdm/lightdm.conf
-echo -e "[Seat:*]\nautologin-user=user\nautologin-user-timeout=0\nautologin-session=cinnamon" >> /etc/lightdm/lightdm.conf
+# TTY AutoLogin
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf >/dev/null <<EOL
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin user --noclear %I linux
+ExecStartPre=-/bin/sh -c "TERM=linux setterm --blank 0 >/dev/tty1"
+EOL
+sudo systemctl enable getty@tty1.service
 
-echo "auth required pam_succeed_if.so user ingroup nopasswdlogin" >> /etc/pam.d/lightdm
+## GUI
+
+# Setup Auto Login in LightDM (for using GUI as default)
+sudo sed -i '/^autologin-user=/d; /^autologin-user-timeout=/d; /^autologin-session=/d' /etc/lightdm/lightdm.conf
+echo -e "[Seat:*]\nautologin-user=user\nautologin-user-timeout=0\nautologin-session=cinnamon" >> sudo tee -a /etc/lightdm/lightdm.conf
+echo "auth required pam_succeed_if.so user ingroup nopasswdlogin" >> sudo tee -a /etc/pam.d/lightdm
 
 groupadd -r autologin
 groupadd -r nopasswdlogin
@@ -52,13 +65,12 @@ gpasswd -a user nopasswdlogin
 rm ~/.local/share/keyrings/*
 
 # Turn off Suspend on Lid Close
-sed -i '/^HandleLidSwitch=/d; /^HandleLidSwitchExternalPower=/d; /^HandleLidSwitchDocked=/d' /etc/systemd/logind.conf
-echo -e "HandleLidSwitch=ignore\nHandleLidSwitchExternalPower=ignore\nHandleLidSwitchDocked=ignore" >> /etc/systemd/logind.conf
-
-systemctl restart systemd-logind.service
+sudo sed -i '/^HandleLidSwitch=/d; /^HandleLidSwitchExternalPower=/d; /^HandleLidSwitchDocked=/d' /etc/systemd/logind.conf
+echo -e "HandleLidSwitch=ignore\nHandleLidSwitchExternalPower=ignore\nHandleLidSwitchDocked=ignore" >> sudo tee -a /etc/systemd/logind.conf
+sudo systemctl restart systemd-logind.service
 
 # Remove Repeated Authentication Prompting in CLI
-echo "user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "user ALL=(ALL) NOPASSWD: ALL" >> sudo tee -a /etc/sudoers
 
 ## Copy Configs from GitHub
 
@@ -84,7 +96,7 @@ python -m venv Documents/Projects/.venv
 pip install --upgrade pip
 
 ## Setup SSH
-systemctl enable sshd
+sudo systemctl enable sshd
 # ssh-keygen -t ed25519 -C "linux-ssh-key"
 # ssh-copy-id shashank@192.168.29.9
 # ip addr show wlan0
