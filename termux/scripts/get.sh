965c3719d4834684d11d762c5668961720484312
Shashank Aditya <msrsaditya@gmail.com>
2025-09-09 15:35:45 +0530

    Create get.sh

---
+++ termux/scripts/get.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+folder="${1:-${HOME}}"
+port=64943
+mkdir -p "${folder}"
+nc -l -w 60 "${port}" | {
+  IFS= read -r file
+  if [[ -z "${file}" ]]; then
+    tar --no-same-owner --warning=no-unknown-keyword --blocking-factor=8192 -xpf - -C "${folder}"
+  else
+    cat > "${folder}/${file}"
+  fi
+}
